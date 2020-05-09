f5dc87ba3e29f149ff3d636256f5549c4a09dc81
Ayman Ghaith <ayman.ghaith88@gmail.com>
2020-05-10 00:20:35 +0300

    implementing heal method

--- app.js
+++ app.js
@@ -4,38 +4,54 @@ new Vue({
         monsterHealth: 100,
         userHealth: 100,
         gameStarted: false,
-        attackLog: []
+        warLog: []
     },
     methods: {
-        startGame: function(){
+        startGame: function() {
             this.gameStarted = true;
             this.userHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function(){
+        attack: function() {
             let userAttack = this.random();
             let monsterAttack = this.random();
-            this.attackLog.push(userAttack, monsterAttack);
+            this.warLog.push(userAttack, monsterAttack);
             this.userHealth -= monsterAttack;
             this.monsterHealth -= userAttack;
-            if (this.userHealth <= 0){
+            this.checkWinner();
+        },
+        specialAttack: function() {
+            
+        },
+        heal: function() {
+            let healingAmount = this.userHealth <= 90 ? 10: 100 - this.userHealth;
+            this.warLog.push(healingAmount);
+            let monsterAttack = this.random();
+            this.userHealth -= monsterAttack;
+            this.warLog.push(monsterAttack);
+            this.checkWinner();
+        },
+        endGame: function() {
+            this.gameStarted = false;
+            
+        },
+        giveUp: function() {
+
+        },
+        random: function() {
+            return Math.floor(Math.random()*10);
+        },
+        checkWinner: function() {
+            if (this.userHealth <= 0) {
                 if (confirm('You lost !!\n\nStart new game?')){
                     this.endGame();
                 };
             }
-            if (this.monsterHealth <= 0){
+            if (this.monsterHealth <= 0) {
                 if (confirm('You winn !!\n\nStart new game?')){
                     this.endGame();
                 };
             }
-
-        },
-        endGame: function(){
-            this.gameStarted = false;
-            
-        },
-        random: function(){
-            return Math.floor(Math.random()*10);
         }
     }
 })
